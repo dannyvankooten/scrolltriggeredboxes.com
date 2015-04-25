8b26582643f5380d2004b085e1ff96011552d745
Danny van Kooten <dannyvankooten@gmail.com>
2015-04-25 22:40:42 +0200

    Rewrote copy on the home and pricing page.

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -13,9 +13,9 @@
 
         <div class="row featurette">
             <div class="col-md-5">
-                <h2 class="featurette-heading">Eye-catching boxes. <span class="text-muted">At just the right time.</span></h2>
-                <p class="lead">With Scroll Triggered Boxes you can boost your conversions by showing your visitors eye-catching boxes at just the right time.</p>
-                <p class="lead">Boxes contain <strong>any</strong> call-to-action you like.</p>
+                <h2 class="featurette-heading">Attention grabbing boxes. <span class="text-muted">At just the right time.</span></h2>
+                <p class="lead">Using Scroll Triggered Boxes, you can ask your visitors to perform a certain action at just the right time.</p>
+                <p class="lead">For example, slide in a sign-up form box once a visitor reaches the end of your post. Already engaged visitors are very likely to want more of your goodness.</p>
             </div>
             <div class="col-md-7">
                 <img class="featurette-image img-responsive center-block" alt="500x500" src="{{ asset('img/screenshots/box.png') }}">
@@ -26,9 +26,9 @@
 
         <div class="row featurette">
             <div class="col-md-5 col-md-push-7">
-                <h2 class="featurette-heading">Effective call to actions. <span class="text-muted">With ease.</span></h2>
-                <p class="lead">Fill the box with whatever you would like to offer or ask your visitors, straight from your WordPress editor.</p>
-                <p class="lead">Use whatever content you like, even third-party plugin shortcodes.</p>
+                <h2 class="featurette-heading">Your own call to actions. <span class="text-muted">With ease.</span></h2>
+                <p class="lead">Use any content you like for your boxes. Whatever you would like to offer or ask your visitors.</p>
+                <p class="lead">Buttons, related posts, forms, even third-party plugin shortcodes. You will be able to see the final result straight from your WordPress visual editor.</p>
             </div>
             <div class="col-md-7 col-md-pull-5">
                 <img class="featurette-image img-responsive center-block" alt="500x500" src="{{ asset('img/screenshots/editor.png') }}">
@@ -39,7 +39,7 @@
 
         <div class="row featurette">
             <div class="col-md-5">
-                <h2 class="featurette-heading">Attract attention. <span class="text-muted">Make your boxes pretty.</span></h2>
+                <h2 class="featurette-heading">Blend in with your theme. <span class="text-muted">Make your boxes pretty.</span></h2>
                 <p class="lead">Finish off your boxes with a pretty design sauce, so the box is truly your own.</p>
                 <p class="lead">A color here, a border there. A sure way to catch the attention of your visitor.</p>
             </div>
--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -6,14 +6,16 @@
     <div class="jumbotron">
         <div class="container">
             <h1>Pricing</h1>
-            <p>The base plugin is and <strong>always will be free</strong> but there are several paid plans available which give you a range of benefits.</p>
+            <p>The Scroll Triggered Boxes core plugin <strong>is and always will be free</strong>.</p>
+            <p>On top of that, there are several paid plans available which give you a range of benefits.</p>
+            <br />
             <div class="row">
                 <div class="col-sm-4 sm-bottom-margin">
                     <div class="pricing-block">
                         <h2>Free</h2>
                         <ul class="list-unstyled">
                             <li class="price">$0</li>
-                            <li>Scroll Triggered Boxes plugin</li>
+                            <li>Scroll Triggered Boxes core plugin</li>
                             <li>Access to all free add-ons</li>
                             <li><a href="https://wordpress.org/plugins/scroll-triggered-boxes/" class="btn btn-lg btn-default"><span class="glyphicon glyphicon-download-alt"></span> Download</a></li>
                         </ul>
@@ -24,10 +26,10 @@
                         <h2>Personal</h2>
                         <ul class="list-unstyled">
                             <li class="price">$29</li>
-                            <li>Scroll Triggered Boxes plugin</li>
-                            <li>Access to <a href="/plugins">all premium add-ons</a></li>
-                            <li>1 year of auto-updates on 1 site</li>
-                            <li>1 year of email support</li>
+                            <li>Scroll Triggered Boxes core plugin</li>
+                            <li>Access to <a href="{{ url('/plugins') }}">all premium add-ons</a></li>
+                            <li>1 year of plugin updates on 1 site</li>
+                            <li>1 year of priority email support</li>
                             <li><a href="https://transactions.sendowl.com/subscriptions/1019/36F66557/purchase" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-shopping-cart"></span> Purchase</a></li>
                         </ul>
                     </div>
@@ -37,10 +39,10 @@
                         <h2>Developer</h2>
                         <ul class="list-unstyled">
                             <li class="price">$49</li>
-                            <li>Scroll Triggered Boxes plugin</li>
-                            <li>Access to <a href="/plugins">all premium add-ons</a></li>
+                            <li>Scroll Triggered Boxes core plugin</li>
+                            <li>Access to <a href="{{ url('/plugins') }}">all premium add-ons</a></li>
                             <li>1 year of plugin updates on 10 sites</li>
-                            <li>1 year of email support</li>
+                            <li>1 year of priority email support </li>
                             <li><a href="https://transactions.sendowl.com/products/169391/230F7370/purchase" class="btn btn-lg btn-primary"><span class="glyphicon glyphicon-shopping-cart"></span> Purchase</a></li>
                         </ul>
                     </div>
@@ -52,12 +54,17 @@
     <div class="container content">
         <div class="row marketing">
             <div class="col-lg-6">
-                <h4>Do I receive updates of the plugins?</h4>
-                <p>Yes! After your payment, you will instantly receive a license key which can be used to configure automatic update checks on 1 or multiple sites (depending on your plan).</p>
+                <h4>Do you have a refund policy?</h4>
+                <p>Definitely! We trust you will enjoy our product so much that <a href="{{ url('/refund-policy') }}">you can get a full refund within 30 days of your purchase</a>, no questions asked.</p>
 
                 <br />
 
-               <h4>Do you offer support when I need help?</h4>
+                <h4>Do I receive updates of the plugins?</h4>
+                <p>Yes! After your payment, you will instantly receive a license key which can be used to configure automatic update checks on one or multiple sites (depending on your plan).</p>
+
+                <br />
+
+                <h4>Do you offer support when I need help?</h4>
                 <p>Of course we do. In fact, we're known for our top-notch support.</p>
                 <p>After purchasing your plan, you will get a dedicated email address that guarantees a fast response.</p>
 
@@ -67,24 +74,26 @@
                 <p>Yes, just <a href="{{ url('/contact') }}">send us an email</a> and we will make it happen.</p>
 
                 <br />
-                <h4>I have another question.</h4>
-                <p>Please <a href="{{ url('/contact') }}">send us an email</a> and we will answer it.</p>
+
+
+
             </div>
 
             <div class="col-lg-6">
-                <h4>Do you have a refund policy?</h4>
-                <p>Definitely! We trust you will enjoy our product so much that <a href="{{ url('/refund-policy') }}">you can get a full refund within 30 days of your purchase</a>, no questions asked.</p>
-
-                <br />
 
                 <h4>Is the price a one-time fee?</h4>
-                <p>Not really. By default, your license will automatically renew itself each year for 50% of the initial purchase price.</p>
-                <p>You can opt-out of license renewals at any time you want, but that means you will no longer receive plugin updates once your license expires.</p>
+                <p>No. By default, your license will automatically renew itself each year for 50% of the initial purchase price.</p>
+
+                <p>You can however opt-out of license renewals at any time you want, but that also means you will no longer receive plugin updates after your license expires.</p>
                 <p>For a multitude of reasons, we believe no one should be on outdated software. This is why we choose to renew licenses by default.</p>
 
                 <br />
                 <h4>What happens after my license expires?</h4>
-                <p>If you canceled your subscriptions then your license will expire after the year has ended. This means that the plugin will keep on working as it is at that moment but you will no longer have access to plugin updates, new plugins and email support.</p>
+                <p>If you cancel your subscription then your license will expire a year after your purchase (or last payment).</p>
+                <p>The plugin will keep on working as it is at that moment but you will no longer have access to plugin updates, new plugins or priority support over email.</p>
+                <br />
+                <h4>I have another question.</h4>
+                <p>Please <a href="{{ url('/contact') }}">send us an email</a> and we will answer it.</p>
 
             </div>
         </div>
