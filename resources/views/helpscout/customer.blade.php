f80eccaf224689ecf0d490f8d71d97bf4dcfcbe0
Danny van Kooten <dannyvankooten@gmail.com>
2015-09-07 15:29:05 +0700

    Improvements to HelpScout Integration.

--- resources/views/helpscout/customer.blade.php
+++ resources/views/helpscout/customer.blade.php
@@ -1,21 +1,23 @@
 @foreach($user->licenses as $license)
     <div class="toggleGroup open">
+        <strong>{{$license->license_key}}</strong> <a class="toggleBtn"><i class="icon-arrow"></i></a>
 
-         <a class="toggleBtn"><i class="icon-arrow"></i></a>
-
-        <strong>{{$license->license_key}}</strong>
-
-        @if(count($license->activations) > 0 )
-        <div class="toggleGroup nested">
-            <a href="" class="toggleBtn"><i class="icon-arrow"></i> Active sites</a>
-            <div class="toggle indent">
-                <ul class="unstyled">
-                @foreach($license->activations as $activation)
-                    <li><a href="{{ $activation->url }}">{{ $activation->domain }}</a></li>
-                @endforeach
-                </ul>
+        <div class="toggle indent">
+            <p>
+                <span class="muted">{{ $license->created_at }}</span>
+            </p>
+            @if(count($license->activations) > 0 )
+            <div class="toggleGroup nested">
+                <a href="" class="toggleBtn"><i class="icon-arrow"></i> Active sites ({{ count( $license->activations ) }}/{{ $license->site_limit }})</a>
+                <div class="toggle indent">
+                    <ul class="unstyled">
+                    @foreach($license->activations as $activation)
+                        <li><a href="{{ $activation->url }}">{{ $activation->domain }}</a></li>
+                    @endforeach
+                    </ul>
+                </div>
             </div>
+            @endif
         </div>
-        @endif
     </div>
 @endforeach
