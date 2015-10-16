e2e4ea7b18164ad1026d2ed33fd5977b04a0e3b8
Danny van Kooten <dannyvankooten@gmail.com>
2015-10-16 11:10:51 +0700

    Add testimonials to home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -48,10 +48,71 @@
             </div>
         </div>
 
-        <hr class="featurette-divider" style="margin-bottom: 0; border-top-color: transparent;">
-
         <!-- /END THE FEATURETTES -->
     </div>
 
+    <div class="medium-margin"></div>
+
     @include('parts.cta')
+
+    <div class="container medium-margin">
+
+        <div class="row testimonials">
+            <div class="col-lg-12">
+
+                <h3 class="section-title">What Users Are Saying</h3>
+
+                <blockquote class="testimonial">
+                    <header>
+                        <img class="avatar" src="{{ asset('img/testimonials/signe-dean.png') }}" height="120" width="120" />
+                    </header>
+                    <p class="copy">
+                        I was looking around for a solution to recruit more newsletter subscribers, and knew I didn't want one of those annoying, obtrusive pop-ups, but a corner scroll-triggered box instead. Man, are those things hard to find. Each plugin that claims to offer this functionality has some fatal flaw. Not this one, though.
+                        <br /><br />
+                        This plugin works perfectly, it is fast, accurate, with just enough customisation, and the shortcode content of my MailPoet newsletter form renders perfectly.
+                    </p>
+                    <footer>
+                        <span class="author-name">Signe Dean</span>, <span class="author-title">freelance science writer & journalist</span>
+                    </footer>
+                </blockquote>
+
+                <blockquote class="testimonial">
+                    <header>
+                        <img class="avatar" src="{{ asset('img/testimonials/graeme.jpeg') }}" height="120" width="120" />
+                    </header>
+                    <p class="copy">
+                        Get this plugin quickly. It's responsible for a huge lift in my conversions. Simply brilliant!
+                    </p>
+                    <footer>
+                        <span class="author-name">Graeme Taylor-Warne</span>, <span class="author-title">internet marketer and co-owner of The Online Dog Trainer</span>
+                    </footer>
+                </blockquote>
+
+                <blockquote class="testimonial">
+                    <header>
+                        <img class="avatar" src="{{ asset('img/testimonials/jerry-davis.jpg') }}" height="120" width="120" />
+                    </header>
+                    <p class="copy">
+                        I have been testing CTA's for a week. One would think that a solid, simple CTA would be common in the plugin environment, but they are not. After a week, and reduced to selecting products to try by working doggedly through a compiled list, I came to this one. It had 64 five-star ratings, and no other ratings of lesser stature. So I tried it and in less than five minutes I had a superior CTA that met all of my needs and more. I am the 65th five-star, and I am grateful.
+                    </p>
+                    <footer>
+                        <span class="author-name">Jerry Davis</span>, <span class="author-title">Managing Director @ BioTech</span>
+                    </footer>
+                </blockquote>
+
+                <blockquote class="testimonial">
+                    <header>
+                        <img class="avatar" src="{{ asset('img/testimonials/vivek.jpeg') }}" height="120" width="120" />
+                    </header>
+                    <p class="copy">
+                        I tested many pop-up plugins, this plugin is the only one that worked easily while not slowing down my site. I highly recommended it!
+                    </p>
+                    <footer>
+                        <span class="author-name">Vivek Srivastava</span>, <span class="author-title">Business Analyst @ Monkshouts</span>
+                    </footer>
+                </blockquote>
+
+            </div>
+        </div><!-- .row -->
+    </div><!-- .container -->
 @stop
