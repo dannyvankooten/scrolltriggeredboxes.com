c02414b93185b0ebfc60a3f98ea5fce92211380a
Danny van Kooten <dannyvankooten@gmail.com>
2016-04-25 14:26:14 +0200

    Add `subscriptions:charge` to command schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -21,15 +21,13 @@ class Kernel extends ConsoleKernel {
 	/**
 	 * Define the application's command schedule.
 	 *
-	 * TODO: Use this over system cronjobs
-	 *
 	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
 	 * @return void
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-//		$schedule->command('inspire')
-//				 ->hourly();
+		$schedule->command('subscriptions:charge')
+				 ->daily();
 	}
 
 }
