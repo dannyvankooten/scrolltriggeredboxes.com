7335a1fd04ffe31670e76cad3694898befc82e97
Danny van Kooten <dannyvankooten@gmail.com>
2016-05-12 16:26:55 +0200

    Simplify email and hint at login credentials.

--- resources/views/emails/license-details.blade.php
+++ resources/views/emails/license-details.blade.php
@@ -1,16 +1,16 @@
 <p>Hey {{ $user->name }},</p>
 <p>Thank you for purchasing a new license!</p>
 
+<h3>Downloading plugins</h3>
+<p>To download any of the premium add-on plugins, <a href="{{ domain_url('/', 'account') }}">log-in to our account area</a> using <strong>{{ $user->email }}</strong> and your chosen password.</p>
+<br />
+
 <h3>Your license key</h3>
-<p>You can use the following license key to configure the plugin for update checks.</p>
+<p>You can use the following key to configure the plugin for update checks.</p>
 <p><code style="font-size: 20px;">{{ $license->license_key  }}</code></p>
 <br />
 
-<h3>Downloading plugins</h3>
-<p>To download any of the premium plugins, please <a href="{{ domain_url('/', 'account') }}">log-in to the account area on our site</a>.</p>
-<br />
-
-<p>Last but not least: if you have any questions, please don't hesitate to let us know. We're here to help!</p>
+<p>If you have any questions, don't hesitate to let us know. We're here to help!</p>
 
 <br />
 <p style="font-style: italic; ">
