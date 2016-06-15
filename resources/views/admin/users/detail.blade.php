9e0ef5fffc0096b87c63b5179c0446d1b7d99089
Danny van Kooten <dannyvankooten@gmail.com>
2016-06-15 15:25:45 +0200

    Improve user details page by showing more details.

--- resources/views/admin/users/detail.blade.php
+++ resources/views/admin/users/detail.blade.php
@@ -10,22 +10,57 @@
             <a href="/users/">Users</a> &rightarrow; {{ $user->email }}
         </div>
 
-        <h1>User {{ $user->email }}</h1>
+        <h1>User <small class="muted">{{ $user->email }}</small></h1>
 
+        <table class="table table-striped">
+            <tr>
+                <th>Email</th>
+                <td>{{ $user->email }}</td>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <td>{{ $user->name }}</td>
+            </tr>
+            <tr>
+                <th>Address</th>
+                <td>{{ $user->getFullAddress() }}</td>
+            </tr>
+            @if( $user->company )
+                <tr>
+                    <th>Company</th>
+                    <td>{{ $user->company }}</td>
+                </tr>
+            @endif
+
+            @if( $user->vat_number )
+                <tr>
+                    <th>VAT Number</th>
+                    <td>{{ $user->vat_number }}</td>
+                </tr>
+            @endif
+
+            <tr>
+                <th>Joined</th>
+                <td>{{ $user->created_at->format('Y-m-d') }}</td>
+            </tr>
+        </table>
+
+
+        <h3>Licenses</h3>
         <table class="table table-striped">
             <thead>
             <tr>
                 <th>License Key</th>
-                <th width="20%">Used on # sites</th>
-                <th>Created at</th>
+                <th width="20%">Activations</th>
+                <th>Created</th>
             </tr>
             </thead>
             <tbody>
             @foreach($user->licenses as $license)
                 <tr>
                     <td><a href="{{ url('/licenses/' . $license->id) }}">{{ $license->license_key }}</a></td>
-                    <td>{{ count( $license->activations ) }}</td>
-                    <td>{{ $license->created_at->format('F d, Y') }}</td>
+                    <td>{{ count( $license->activations ) .'/' . $license->site_limit }}</td>
+                    <td>{{ $license->created_at->format('Y-m-d') }}</td>
                 </tr>
             @endforeach
             </tbody>
