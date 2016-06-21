483d078ef091c0dd54d764eac04cfb2da7e737c4
Danny van Kooten <dannyvankooten@gmail.com>
2016-06-21 09:35:42 +0200

    Don't run `subscriptions:charge` command at midnight but at 5AM instead.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel {
 	protected function schedule(Schedule $schedule)
 	{
 		$schedule->command('subscriptions:charge')
-				 ->daily();
+				 ->dailyAt('05:00');
 	}
 
 }
