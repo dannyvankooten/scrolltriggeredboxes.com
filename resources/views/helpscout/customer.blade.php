698bb7ce971aaa83891458803a29bcd4431e0b7a
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-05 16:03:49 +0200

    Improve HelpScout HTML response with links to admin area.

--- resources/views/helpscout/customer.blade.php
+++ resources/views/helpscout/customer.blade.php
@@ -1,23 +1,33 @@
-@foreach($user->licenses as $license)
-    <div class="toggleGroup open">
-        <strong>{{$license->license_key}}</strong> <a class="toggleBtn"><i class="icon-arrow"></i></a>
 
-        <div class="toggle indent">
-            <p>
-                <span class="muted">{{ $license->created_at }}</span> <br />
-            </p>
-            @if(count($license->activations) > 0 )
-            <div class="toggleGroup nested">
-                <a href="" class="toggleBtn"><i class="icon-arrow"></i> Active sites ({{ count( $license->activations ) }}/{{ $license->site_limit }})</a>
-                <div class="toggle indent">
-                    <ul class="unstyled">
-                    @foreach($license->activations as $activation)
-                        <li><a href="{{ $activation->url }}">{{ $activation->domain }}</a></li>
-                    @endforeach
-                    </ul>
-                </div>
-            </div>
+<p>
+    <a href="{{ domain_url( '/users/' . $user->id, 'admin' ) }}">{{ $user->name }}</a> &lt;{{ $user->email }}&gt; <br />
+    <span class="muted">User since {{ $user->created_at->format( 'j M Y') }}.</span><br />
+    <strong>Lifetime Value:</strong> ${{ $user->getLifetimeValue() }}<br />
+</p>
+
+<ul class="unstyled">
+@foreach($user->licenses as $license)
+    <li>
+        <p>
+            <a href="{{ domain_url( '/licenses/' . $license->id, 'admin' ) }}">{{$license->license_key}}</a><br />
+            <span class="muted">{{ $license->created_at->format( 'j M Y H:i' ) }}</span><br />
+            @if($license->isExpired())
+                <span style="color:orange; font-weight:bold;">expired</span><br />
             @endif
+        </p>
+
+        @if(count($license->activations) > 0 )
+        <div class="toggleGroup">
+            <a href="" class="toggleBtn"><i class="icon-arrow"></i> Active sites ({{ count( $license->activations ) }}/{{ $license->site_limit }})</a>
+            <div class="toggle indent">
+                <ul class="unstyled">
+                @foreach($license->activations as $activation)
+                    <li><a href="{{ $activation->url }}">{{ $activation->domain }}</a></li>
+                @endforeach
+                </ul>
+            </div>
         </div>
-    </div>
+        @endif
+    </li>
 @endforeach
+</ul>
