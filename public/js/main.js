9ab724bcfc92ab6dfc567be75098c902362d1c23
Danny van Kooten <dannyvankooten@gmail.com>
2016-08-16 13:38:26 +0200

    Add `[data-show-if]` script.

--- public/js/main.js
+++ public/js/main.js
@@ -347,6 +347,29 @@ var countryInputs = document.querySelectorAll('.country-input');
 var pricingForms = document.querySelectorAll('form[data-pricing]');
 var confirmationElements = document.querySelectorAll('[data-confirm]');
 var dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+var optionalElements = document.querySelectorAll('[data-show-if]');
+
+function showIf(el, expectedValue) {
+    return function () {
+        var value = this.form.elements.namedItem(this.name).value;
+        var conditionMet = value === expectedValue || expectedValue === "" && value.length > 0;
+        el.style.display = conditionMet ? '' : 'none';
+    };
+}
+
+// hide fields with [data-show-if] attribute
+[].forEach.call(optionalElements, function (el) {
+    var condition = el.getAttribute('data-show-if').split(':');
+    var fields = document.querySelectorAll('[name="' + condition[0] + '"]');
+    var expectedValue = condition[1] || "";
+    var callback = showIf(el, expectedValue);
+
+    for (var i = 0; i < fields.length; i++) {
+        fields[i].addEventListener('change', callback);
+        fields[i].addEventListener('keyup', callback);
+        callback.call(fields[i]);
+    }
+});
 
 var askForConfirmation = function askForConfirmation(event) {
     var sure = confirm(this.getAttribute('data-confirm'));
