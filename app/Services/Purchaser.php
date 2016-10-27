50ac7e778fec624b5089c6d2d151f64f570a1224
Danny van Kooten <dannyvankooten@gmail.com>
2016-10-27 12:53:32 +0200

    When purchasing new license, subscribe user to Stripe subscription and store ID in licenses table.

--- app/Services/Purchaser.php
+++ app/Services/Purchaser.php
@@ -43,56 +43,68 @@ class Purchaser {
     }
 
     /**
-     * @param float $quantity
-     * @param float $interval
+     * @param string $plan
+     * @param string $interval
      *
-     * @return int
+     * @return float
      */
-    public function calculatePrice( $quantity, $interval ) {
-        $base_price = $interval == 'month' ? 4 : 40;
-        $unit_price = 0.5 * $base_price;
-        $amount = $base_price + ( $unit_price * $quantity );
-        $amount = round( $amount, 2 );
-        return $amount;
+    public function calculatePrice( $plan, $interval ) {
+        $prices = array(
+            'personal' => 6.00,
+            'developer' => 20.00,
+        );
+
+        if( ! isset( $prices[ $plan ] ) ) {
+            throw new \InvalidArgumentException( "Invalid plan ID: $plan" );
+        }
+
+        $price = $prices[ $plan ];
+        $yearly = $interval === 'year';
+
+        // a year costs 10 months (2 free months)
+        if( $yearly ) {
+            $price = $price * 10;
+        }
+
+        return $price;
     }
 
     /**
      * @param User $user
-     * @param $quantity
-     * @param $interval
+     * @param string $plan
+     * @param string $interval
      *
      * @return License
      */
-    public function license( User $user, $quantity, $interval )
+    public function license( User $user, $plan, $interval )
     {
-        // subtotal
-        $amount = $this->calculatePrice( $quantity, $interval );
+        if( ! in_array( $plan, array( 'personal', 'developer' ) ) ) {
+            throw new \InvalidArgumentException("Invalid plan ID: $plan");
+        }
 
-        // charge user
-        $payment = $this->charger->charge( $user, $amount );
+        // subscribe user to plan in Stripe
+        $plan_id = sprintf( 'boxzilla-%s-%sly', $plan, $interval );
+        $subscription = \Stripe\Subscription::create([
+            'customer' => $user->stripe_customer_id,
+            'plan' => $plan_id
+        ]);
+
+        $limits = array(
+            'personal' => 2,
+            'developer' => 10
+        );
+        $site_limit = $limits[ $plan ];
 
         // Create license.
         $license = new License();
         $license->license_key = License::generateKey();
         $license->user_id = $user->id;
-        $license->site_limit = $quantity;
+        $license->site_limit = $site_limit;
         $license->expires_at = new DateTime("+1 $interval");
+        $license->auto_renews = true;
+        $license->stripe_subscription_id = $subscription->id;
+        $license->interval = $interval;
         $license->save();
-
-        // Create subscription
-        $subscription = new Subscription([
-            'interval' => $interval,
-            'active' => 1,
-            'next_charge_at' => $license->expires_at->modify('-5 days')
-        ]);
-        $subscription->amount = $amount;
-        $subscription->license_id = $license->id;
-        $subscription->user_id = $user->id;
-        $subscription->save();
-
-        // Attach payment to subscription
-        $payment->subscription_id = $subscription->id;
-        $payment->save();
 
         // dispatch job to send license details over email
         $this->dispatch( new EmailLicenseDetails( $license ) );
